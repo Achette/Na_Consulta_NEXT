0e9283a81229d5c36c58545f66fb51ddf9f29ff2
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-18 17:16:44 -0300

    Feat/task 0001 - Instala e configura styled components com typescript (#3)
    * refactor(TASK-0001): remove códigos e arquivos
    
    * feature(TASK-0001): cria colors default
    
    * feature(TASK-0001): tipagem para styled components
    
    * feature(TASK-0001): cria head para tipografia
    
    * feat(TASK-0001): theme provider e global style
    
    * build(TASK-0001): config de typescript
    
    * refactor(TASK-0001): reitra código

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "src/styles/theme/default";
+import { GlobalStyle } from "src/styles/global";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
