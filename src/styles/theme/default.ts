d6b394b82d9fa1c7b7380a044a5a2f1ac796542d
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-18 15:30:35 -0300

    feature(TASK-0001): cria colors default

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  white: "#fff",
+  black: "#000",
+  gray: "#495057",
+
+  "green-300": "#3DCCB7;",
+  "green-500": "#008F79",
+  "green-700": "#004238",
+} as const;
