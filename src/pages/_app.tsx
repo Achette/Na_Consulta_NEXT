69d1b97d8db32859bf976c7e424746f68d196ca9
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-18 16:05:41 -0300

    feat(TASK-0001): theme provider e global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "src/styles/theme/default";
+import { GlobalStyle } from "src/styles/global";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
