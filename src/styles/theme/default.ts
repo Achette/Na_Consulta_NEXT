67f67f4205179d059a1405095c79b109d36cceaa
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-19 19:09:21 -0300

    refactor(TASK-0006): ajuste no código

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -3,7 +3,7 @@ export const defaultTheme = {
   black: '#000',
   gray: '#495057',
 
-  'green-300': '#3DCCB7;',
+  'green-300': '#3DCCB7',
   'green-500': '#008F79',
   'green-700': '#004238',
 } as const
