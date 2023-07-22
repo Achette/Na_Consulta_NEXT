2cd8e241ef7bcab893d0613cada34f4264ae4b47
Achette <i.achette@live.com>
2023-07-22 14:12:55 -0300

    refact/task-0011: altera fonte do projeto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,15 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='pt-br'>
       <Head>
         <meta charSet="UTF-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
