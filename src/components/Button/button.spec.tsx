b138333cf59ba5fc164977576248d90816285824
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-22 01:49:33 -0300

    refactor(DT-0010): insere describe

--- src/components/Button/button.spec.tsx
+++ src/components/Button/button.spec.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 import { Button } from './index'
 
-test('renders button with correct title and size', () => {
-  const title = 'Click Me!'
-  const size = 'medium'
+describe('Button', () => {
+  it('renders button with correct title and size', () => {
+    const title = 'Click Me!'
+    const size = 'medium'
 
-  const { getByText, getByTestId } = render(
-    <Button title={title} size={size} />,
-  )
+    const { getByText, getByTestId } = render(
+      <Button title={title} size={size} />,
+    )
 
-  expect(getByText(title)).toBeInTheDocument()
-  expect(getByTestId('button-container')).toHaveAttribute('variant', size)
+    expect(getByText(title)).toBeInTheDocument()
+    expect(getByTestId('button-container')).toHaveAttribute('variant', size)
+  })
 })
