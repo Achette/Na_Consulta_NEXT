f96b6d09ba538349f693f60b32b54025cd693253
Achette <i.achette@live.com>
2023-07-23 11:46:46 -0300

    feat/task-0011: cria componente header com responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,60 @@
-import { Logo } from "../Logo";
-import { ContainerHeader, LogoHeaderContainer, LogoText } from "./styles";
+import React from 'react'
+import { Logo } from '../Logo'
+import {
+  LeftContainer,
+  LogoContainer,
+  LogoText,
+  NavbarContainer,
+  NavbarExtendedContainer,
+  NavbarInnerContainer,
+  NavbarLink,
+  NavbarLinkContainer,
+  NavbarLinkExtended,
+  OpenLinksButton,
+  RightContainer,
+} from './styles'
+import { OutlineButton } from '../OutlineButton'
 
 export const Header = () => {
+  const [extendNavbar, setExtendNavbar] = React.useState<boolean>(false)
+
   return (
-    <ContainerHeader>
-      <LogoHeaderContainer>
-        <Logo />
-        <LogoText>NaConsulta</LogoText>
-      </LogoHeaderContainer>
-    </ContainerHeader>
-  );
-};
+    <NavbarContainer extendNavbar={extendNavbar}>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <LogoContainer>
+            <Logo />
+            <LogoText>NaConsulta</LogoText>
+          </LogoContainer>
+        </LeftContainer>
+        <RightContainer>
+          <NavbarLinkContainer>
+            <NavbarLink href="#home">Inicio</NavbarLink>
+            <NavbarLink href="#about">Sobre</NavbarLink>
+            <NavbarLink href="#depositions">Depoimentos</NavbarLink>
+            <NavbarLink href="#talkUs">Fale Conosco</NavbarLink>
+            <OutlineButton />
+            <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((prev) => !prev)
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+            </OpenLinksButton>
+          </NavbarLinkContainer>
+        </RightContainer>
+      </NavbarInnerContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended href="#home">Inicio</NavbarLinkExtended>
+          <NavbarLinkExtended href="#about">Sobre</NavbarLinkExtended>
+          <NavbarLinkExtended href="#depositions">
+            Depoimentos
+          </NavbarLinkExtended>
+          <NavbarLinkExtended href="#talkUs">Fale Conosco</NavbarLinkExtended>
+          <NavbarLinkExtended href="#">AGENDAR CONSULTA</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+    </NavbarContainer>
+  )
+}
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,26 +1,130 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
-export const ContainerHeader = styled.header`
+export const NavbarContainer = styled.nav<{ extendNavbar?: boolean }>`
   width: 100%;
-  height: 7.5rem;
+  height: ${(props) => (props.extendNavbar ? '100vh' : '5rem')};
   background: #3dccb7;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    height: 9.125rem;
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media (min-width: 1400px) {
+    padding: 0 8.125rem;
+  }
 `
 
-export const LogoHeaderContainer = styled.div`
-  width: 20.625rem;
-  height: 4.5rem;
-  border: 1px solid red;
+export const LeftContainer = styled.div`
+  flex: 70%;
+  display: flex;
+  align-items: center;
+  padding-left: 1rem;
+
+  @media (min-width: 768px) {
+    flex: 20%;
+  }
+`
+
+export const RightContainer = styled.div`
+  flex: 30%;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 1rem;
+
+  @media (min-width: 768px) {
+    flex: 80%;
+  }
+`
+
+export const NavbarInnerContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+`
+
+export const NavbarLinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (min-width: 1200px) {
+    width: 52.25rem;
+    justify-content: space-between;
+  }
+`
+
+export const NavbarLink = styled.a`
+  color: white;
+  font-size: 0.85rem;
+  text-decoration: none;
+  margin: 0.625rem;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 1rem;
+  }
+
+  @media (min-width: 1200px) {
+    font-size: 1.5rem;
+  }
+`
+
+export const NavbarLinkExtended = styled.a`
+  color: white;
+  font-size: x-large;
+  text-decoration: none;
+  margin: 0.625rem;
+`
+
+export const OpenLinksButton = styled.button`
+  width: auto;
+  height: auto;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 2.75rem;
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+export const NavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+export const LogoContainer = styled.div`
+  width: 14rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 1rem;
+
+  @media (min-width: 992px) {
+    width: 20.625rem;
+  }
 `
 
 export const LogoText = styled.h1`
-color: ${(props) => props.theme.white};
-font-size: 40px;
-font-style: normal;
-font-weight: 600;
-line-height: normal;
+  color: ${(props) => props.theme.white};
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+
+  @media (min-width: 992px) {
+    font-size: 2.5rem;
+  }
 `
