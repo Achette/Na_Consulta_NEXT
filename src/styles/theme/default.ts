c027d923e9b910a69689888776927e0d898ba00b
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-25 11:49:55 -0300

    refactor(TASK-0013): organiza código

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,9 +1,9 @@
 export const defaultTheme = {
   white: '#fff',
-  black: '#000',
-  gray: '#495057',
-
   light: '#F7F9F9',
+  gray: '#495057',
+  black: '#000',
+
   'green-300': '#3DCCB7',
   'green-500': '#008F79',
   'green-700': '#004238',
