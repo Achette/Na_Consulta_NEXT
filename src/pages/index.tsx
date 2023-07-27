6baece4aaca34085b09a305a4b26e942e47f6d84
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-27 01:19:55 -0300

    feat(TASK-0014): importa componente About Section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Header } from 'src/components/Header'
 import { MainContainer } from './styles'
 import { ServiceCardSection } from 'src/components/CardsGrid'
+import { AboutSection } from 'src/components/About'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <MainContainer>
         <Header />
         <ServiceCardSection />
+        <AboutSection />
       </MainContainer>
     </div>
   )
