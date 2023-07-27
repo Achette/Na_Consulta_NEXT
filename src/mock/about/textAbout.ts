da371431e9d7f3fc023720358a3818a27627e26e
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-27 01:20:28 -0300

    feat(TASK-0014): cria mock de texto

---
+++ src/mock/about/textAbout.ts
@@ -0,0 +1,9 @@
+export const about = `Somos o Na Consulta, uma equipe dedicada em cuidar da sua saúde. Nossa
+missão é facilitar o acesso a informações médicas confiáveis e
+conectar você aos melhores especialistas na área. Acreditamos que a
+saúde é o bem mais precioso e, por isso, existimos para tornar as
+consultas médicas mais acessíveis, humanas e eficientes. Nossa
+plataforma busca proporcionar a melhor experiência, com agendamento
+simplificado e atendimento de qualidade, garantindo que você receba o
+cuidado que merece. Confie em nós para cuidar da sua saúde e
+bem-estar. Juntos, vamos construir um futuro mais saudável!`
