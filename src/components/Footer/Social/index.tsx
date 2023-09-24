033c6ea14bd29a7a86265707bea936346ae30643
Paulo Moutinho <paulo_vicali@icloud.com>
2023-09-23 21:03:13 -0300

    test(TASK-0016): refatora/insere test id

--- src/components/Footer/Social/index.tsx
+++ src/components/Footer/Social/index.tsx
@@ -10,15 +10,17 @@ import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react'
 
 export const Social = () => {
   return (
-    <SocialContainer>
-      <SocialHeader>
+    <SocialContainer data-testid="social-container">
+      <SocialHeader data-testid="social-header">
         <Logo />
-        <SocialLogoText>NaConsulta</SocialLogoText>
+        <SocialLogoText data-testid="social-logo-text">
+          NaConsulta
+        </SocialLogoText>
       </SocialHeader>
-      <SocialSloganText>
+      <SocialSloganText data-testid="social-slogan-text">
         O jeito fácil de agendar consultas médica
       </SocialSloganText>
-      <SocailIcons>
+      <SocailIcons data-testid="social-social-icons">
         <Facebook />
         <Instagram />
         <Twitter />
